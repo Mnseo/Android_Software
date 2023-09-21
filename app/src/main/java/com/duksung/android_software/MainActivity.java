7581801bdb0ebf7d0d35622685046b807456c5b4
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-09-22 01:46:39 +0900

    Add: Xml file Add

--- app/src/main/java/com/duksung/android_software/MainActivity.java
+++ app/src/main/java/com/duksung/android_software/MainActivity.java
@@ -2,13 +2,26 @@ package com.duksung.android_software;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button btn1, btn2;
+    RadioButton radio1, radio2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
