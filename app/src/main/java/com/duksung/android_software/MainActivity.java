287e706e27df15c317e182f2ee571750b2085cb3
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-09-22 03:23:17 +0900

    Week03/Assignment

--- app/src/main/java/com/duksung/android_software/MainActivity.java
+++ app/src/main/java/com/duksung/android_software/MainActivity.java
@@ -6,18 +6,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button btn1, btn2;
-    RadioButton radio1, radio2;
+    RadioGroup radioGroup;
     ImageView imageView;
 
     @Override
@@ -28,41 +30,47 @@ public class MainActivity extends AppCompatActivity {
         //xml - 부착
         editText = (EditText) findViewById(R.id.editText);
         btn1 = (Button) findViewById(R.id.btn1); btn2 = (Button) findViewById(R.id.btn2);
-        radio1 = (RadioButton) findViewById(R.id.radio1); radio2 = (RadioButton) findViewById(R.id.radio2);
+        radioGroup = findViewById(R.id.radioGroup);
         imageView = (ImageView) findViewById(R.id.imageView);
 
         View.OnClickListener buttonClickListener = view -> {
             String text = editText.getText().toString().trim();
-            String url = "";
             switch (view.getId()) {
                 case R.id.btn1:
                     // btn1이 클릭됐을 때의 동작
                     Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.btn2:
-                    //주소창으로 옮길 수 있는지 확인
-                    if (!text.startsWith("http://") && !text.startsWith("https://")) {
-                        url = editText.getText().toString().trim();
-                    }
                     // btn2가 클릭됐을 때의 동작 (암시적 인텐트)
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                    startActivity(intent);
-                    break;
-
-                //Radio 버튼 객체 클릭 이벤트
-                case R.id.radio1:
-                    imageView.setImageResource(R.drawable.oreo);
-                    imageView.setVisibility(View.VISIBLE);
-                case R.id.radio2:
-                    imageView.setImageResource(R.drawable.pie);
-                    imageView.setVisibility(View.VISIBLE);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(text));
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(this, "No application can handle this URL!", Toast.LENGTH_SHORT).show();
+                    }
             }
         };
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radio1:
+                        imageView.setImageResource(R.drawable.oreo);
+                        imageView.setVisibility(View.VISIBLE);
+                        Log.d("image Resource", "radio1");
+                        break;
+                    case R.id.radio2:
+                        imageView.setImageResource(R.drawable.pie);
+                        imageView.setVisibility(View.VISIBLE);
+                        Log.d("image Resource", "radio2");
+                        break;
+                }
+            }
+        });
 
         btn1.setOnClickListener(buttonClickListener);
         btn2.setOnClickListener(buttonClickListener);
 
-        radio1.setOnClickListener(buttonClickListener);
-        radio2.setOnClickListener(buttonClickListener);
     }
 }
