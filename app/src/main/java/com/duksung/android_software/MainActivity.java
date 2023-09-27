a4cae6e0119aabda918ebc143c2f48a4a5763b28
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-09-27 20:54:40 +0900

    Add: xml and memoActivity

--- app/src/main/java/com/duksung/android_software/MainActivity.java
+++ app/src/main/java/com/duksung/android_software/MainActivity.java
@@ -1,47 +1,96 @@
 package com.duksung.android_software;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.TimePicker;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MemoActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    RadioButton rdoDate, rdoTime;
+    DatePicker datePicker;
+    TimePicker timePicker;
+    String date, time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_memo);
-        android.util.Log.i("lifecycle", "Memo:onCreate");
 
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        Button button = (Button) findViewById(R.id.button);
-        final EditText editText = (EditText) findViewById(R.id.editText);
+        setContentView(R.layout.activity_main);
+//        Log.i("lifecycle", "Main:onCreate");
+
+        button = (Button) findViewById(R.id.button);
+        rdoDate = (RadioButton) findViewById(R.id.radioButton);
+        rdoTime = (RadioButton) findViewById(R.id.radioButton2);
+
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        // 처음에는 두 picker를 안보이게 설정
+        timePicker.setVisibility(View.INVISIBLE);
+        datePicker.setVisibility(View.INVISIBLE);
+
+        rdoDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                timePicker.setVisibility(View.INVISIBLE);
+                datePicker.setVisibility(View.VISIBLE);
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                timePicker.setVisibility(View.VISIBLE);
+                datePicker.setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+                @Override
+                public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                    date = i + "/" + (i1+1) + "/" + i2;
+                }
+            });
+        }
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = getIntent();
-                String appointment = intent.getStringExtra("appointment");
-                String memo = editText.getText().toString();
-                textView.setText(memo + " : " + appointment);
+                time = timePicker.getHour() + ":" + timePicker.getMinute();
+                String appointment = date + " " + time;
+                Toast.makeText(getApplicationContext(), appointment, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), MemoActivity.class);
+                intent.putExtra("appointment", appointment);
+                startActivity(intent);
             }
         });
-
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        android.util.Log.i("lifecycle", "Memo:onResume");
+        android.util.Log.i("lifecycle", "Main:onResume");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        android.util.Log.i("lifecycle", "Memo:onStop");
+        android.util.Log.i("lifecycle", "Main:onStop");
     }
-}
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        android.util.Log.i("lifecycle", "Main:onRestart");
+    }
+
+}
