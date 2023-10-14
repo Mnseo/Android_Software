f91d11c3dd12ef77fe0d67e7823cbe89cb418774
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-10-14 16:14:11 +0900

    Mod: Card App(5장만 뽑기) Code

--- app/src/main/java/com/duksung/android_software/MainActivity.java
+++ app/src/main/java/com/duksung/android_software/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,12 +18,24 @@ public class MainActivity extends AppCompatActivity {
         Suit suits[] = Suit.values();
         Rank ranks[] = Rank.values();
         ArrayList<Card> cards = new ArrayList<Card>();
-        // card 5장만 생성
-        for (int i = 0; i < 5; i++) {
-            Card c = new Card(suits[i % 4], ranks[i % 13]);
+
+        // 각 무늬 별로 카드 수를 저장하는 배열
+        int[] suitCounts = new int[suits.length];
+
+        // 원하는 카드 수만큼 생성
+        int totalCardsToGenerate = 5; // 원하는 카드 수
+        for (int i = 0; i < totalCardsToGenerate; i++) {
+            Suit currentSuit = suits[i % 4];
+            Rank currentRank = ranks[i % 13];
+
+            // 해당 무늬의 카드 수 증가
+            suitCounts[currentSuit.ordinal()]++;
+
+            Card c = new Card(currentSuit, currentRank);
             c.flip();
             cards.add(c);
         }
+
         for(Card c : cards) {
             System.out.println(c.description());
         }
@@ -34,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
         int suit_value = cards.get(0).suit.ordinal() + 1;
         int rank_value = cards.get(0).rank.ordinal() + 1;
         System.out.println("첫번째 카드의 무늬값:" + suit_value + "/숫자값:" + rank_value);
+
+        // 각 무늬별로 남은 카드 수 출력
+        for (Suit suit : Suit.values()) {
+            int remainingCards = totalCardsToGenerate / 4 - suitCounts[suit.ordinal()];
+            System.out.println("무늬 " + suit + "의 남은 카드 수: " + remainingCards);
+        }
 
     }
 
