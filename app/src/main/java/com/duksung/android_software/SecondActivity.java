77967d8f3eb240942c46bee3277711375df5787c
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-10-16 12:37:02 +0900

    chore: clear main code

--- app/src/main/java/com/duksung/android_software/SecondActivity.java
+++ app/src/main/java/com/duksung/android_software/SecondActivity.java
@@ -7,30 +7,11 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-    TextView winnerTv1, winnerTv2, winnerTv3;
-    int yourScore, myScore;
-    String winnerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-        winnerTv1 = (TextView) findViewById(R.id.winner_tv1);
-        winnerTv2 = (TextView) findViewById(R.id.winner_tv2);
-        winnerTv3 = (TextView) findViewById(R.id.winner_tv3);
-
-        yourScore = getIntent().getIntExtra("yourScore", 0);
-        myScore = getIntent().getIntExtra("myScore", 0);
-
-        winnerTv2.setText("your score: " + yourScore);
-        winnerTv3.setText("my score: " + myScore);
-
-        if (yourScore > myScore) { winnerText = "You Won!";}
-        else if (yourScore < myScore) { winnerText = "I Won!"; }
-        else if (yourScore == myScore) { winnerText = "Draw!"; }
-
-        winnerTv1.setText(winnerText);
 
 
     }
