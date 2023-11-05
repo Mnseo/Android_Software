67979de281d073bb1baac8b2a3b332f40a36df95
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-11-05 17:08:42 +0900

    Finish: Assignment

--- app/src/main/java/com/duksung/android_software/DetailActivity.java
+++ app/src/main/java/com/duksung/android_software/DetailActivity.java
@@ -1,8 +1,8 @@
 package com.duksung.android_software;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
     Integer[] meals = {R.drawable.meal0, R.drawable.meal1, R.drawable.meal2};
+    String mealName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +24,17 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Integer position = intent.getIntExtra("meal", 0);
-        String mealName = intent.getStringExtra("meal_name");
+        mealName = intent.getStringExtra("meal_name");
         imageView.setImageResource(meals[position]);
         textView.setText(mealName);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        button.setOnClickListener(view -> {
+            String url = "https://www.google.com/search?q=" + Uri.encode(mealName);
+            Intent intent1 = new Intent(Intent.ACTION_VIEW);
+            intent1.setData(Uri.parse(url));
+            startActivity(intent1);
 
-            }
         });
     }
 }
