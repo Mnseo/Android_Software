eb74cfb8dd4a094bf9bafe9b09cc9ed0c59a47de
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-11-15 04:07:53 +0900

    week11 Assignment Finish

--- app/src/main/java/com/duksung/android_software/DetailActivity.java
+++ app/src/main/java/com/duksung/android_software/DetailActivity.java
@@ -15,34 +15,8 @@ import java.util.List;
 
 
 public class DetailActivity extends AppCompatActivity {
-    Integer[] meals = {R.drawable.meal0, R.drawable.meal1, R.drawable.meal2};
-    String mealName = "";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
-
-//        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        TextView textView = (TextView)findViewById(R.id.textView);
-        Button button = (Button) findViewById(R.id.button);
-
-
-
-        Intent intent = getIntent();
-        Integer position = intent.getIntExtra("meal", 0);
-        mealName = intent.getStringExtra("meal_name");
-//        imageView.setImageResource(meals[position]);
-        textView.setText(mealName + "선택! \n 추가정보를 원하시면 More 버튼 클릭! ");
-
-
-        button.setOnClickListener(view -> {
-            Intent intent1 = new Intent();
-            intent1.setAction(Intent.ACTION_WEB_SEARCH);
-            Log.d("meal_Name", mealName);
-            intent1.putExtra(SearchManager.QUERY, mealName);
-            startActivity(intent1);
-
-        });
     }
 }
