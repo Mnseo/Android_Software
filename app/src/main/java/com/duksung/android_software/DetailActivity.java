b3ccb7b1c3483d309492502ab5d3735e98a220fd
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-12-03 00:22:12 +0900

    Finish: Assignment 13

--- app/src/main/java/com/duksung/android_software/DetailActivity.java
+++ app/src/main/java/com/duksung/android_software/DetailActivity.java
@@ -1,22 +1,84 @@
 package com.duksung.android_software;
 
-import android.app.SearchManager;
+
 import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class DetailActivity extends AppCompatActivity {
+    private Document doc;
+    ArrayList<String> employee;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        employee = new ArrayList<String>();
+        ListView listView = (ListView)findViewById(R.id.listView1);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, employee);
+        listView.setAdapter(adapter);
+
+        String urlString = "http://203.252.213.36:8080/FinalProject/advisorPro2.jsp";
+        Intent intent = getIntent();
+        String professor = intent.getStringExtra("professor");
+        urlString = urlString + "?advisor=" + professor;
+        Log.d("url", urlString);
+        JsoupAsyncTask task = new JsoupAsyncTask();
+        task.execute(urlString);
+    }
+
+    private class JsoupAsyncTask extends AsyncTask<String, Void, Document> {
+        private Exception exception = null;
+        @Override
+        protected Document doInBackground(String... params) {;
+            try {
+                doc = Jsoup.connect(params[0]).get();
+                Log.d("doInbackground", "try 성공");
+            } catch (Exception e) {
+                exception = e;
+                Log.d("doInbackground", "Exception: " + e.getMessage());
+                Log.d("doInbackground", "catch 성공");
+            }
+            return doc;
+        }
+
+        @Override
+        protected void onPostExecute(Document doc) {
+            if(exception != null) {
+                Toast.makeText(getBaseContext(), "network error", Toast.LENGTH_SHORT).show();
+            } else {
+                if (doc != null) {
+                    Elements h5_elements = doc.select("h5");
+                    Elements i_elements = doc.select("i");
+                    for (int i = 0; i < h5_elements.size(); i++) {
+                        String h5Text = h5_elements.get(i).text();
+                        String iText = (i_elements.size() > i) ? i_elements.get(i).text() : "";
+                        String combinedText = h5Text + " / " + iText;
+
+                        employee.add(combinedText);
+                        Log.d("pass", combinedText);
+                    }
+                    adapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(getBaseContext(), "doc is null", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/duksung/android_software/MainActivity.java
+++ app/src/main/java/com/duksung/android_software/MainActivity.java
@@ -1,46 +1,46 @@
 package com.duksung.android_software;
 
-import androidx.appcompat.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-    String [] professors = {"음두헌", "이형규", "이재호", "강지헌", "유제혁"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ListView professorList = (ListView) findViewById(R.id.listView);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, professors);
-
-        professorList.setAdapter(adapter);
-
-        professorList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), professors[i], Toast.LENGTH_LONG).show();
-
-                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
-                intent.putExtra("professor", professors[i]);
-                startActivity(intent);
-            }
-        });
-
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nativeApp:
+                // nativeApp 메뉴 아이템을 클릭했을 때
+                Intent intent1 = new Intent(this, ProfessorActivity.class);
+                startActivity(intent1);
+                return true;
+
+            case R.id.WebApp:
+                // WebApp 메뉴 아이템을 클릭했을 때
+                String url = "http://203.252.213.36:8080/FinalProject/advisorForm.jsp";
+                Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//                intent2.setData(Uri.parse(url));
+                startActivity(intent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
